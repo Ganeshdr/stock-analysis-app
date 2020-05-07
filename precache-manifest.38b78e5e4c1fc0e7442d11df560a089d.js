f7ec2fe9f251cb3406e9977373fbaccca374cd7b
grevanak <grevanak@akamai.com>
2020-05-07 19:04:35 +0530

    Updates

---
+++ precache-manifest.38b78e5e4c1fc0e7442d11df560a089d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4dcd532f9e06fe8b277e5b058ca571f",
+    "url": "/stock-analysis-app/index.html"
+  },
+  {
+    "revision": "efc289d2bf5cf20aeb42",
+    "url": "/stock-analysis-app/static/css/main.52134d8e.chunk.css"
+  },
+  {
+    "revision": "039dd2b50f5ea767f0cb",
+    "url": "/stock-analysis-app/static/js/2.ea05de10.chunk.js"
+  },
+  {
+    "revision": "2af0ab6b89712f92f0eda55a98b3df08",
+    "url": "/stock-analysis-app/static/js/2.ea05de10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc289d2bf5cf20aeb42",
+    "url": "/stock-analysis-app/static/js/main.8733d79b.chunk.js"
+  },
+  {
+    "revision": "fda10a66ba96d4eb6e4b",
+    "url": "/stock-analysis-app/static/js/runtime-main.7a4b76e1.js"
+  }
+]);
